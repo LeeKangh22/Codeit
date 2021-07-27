d66daaa3121764106582f8ff31df4ae79de2671f
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-07-27 20:54:27 +0900

    2021/7/27

---
+++ LinkedList.py
@@ -0,0 +1,56 @@
+# Node class
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_after(self, previous_node, data):
+        new_node = Node(data)
+        
+        if previous_node is self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            new_node.next = previous_node.next
+            previous_node.next = new_node
+
+    def get_node(self, index):
+        iterator = self.head
+        
+        for i in range(0,index):
+            iterator = iterator.next
+        return iterator
+
+    def append(self, data):
+        newNode = Node(data)
+
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+# Make new linked list 
+my_list = LinkedList()
+
+# Append data
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+my_list.append(7)
+node_1 = my_list.get_node(2)
+my_list.insert_after(node_1, 6)
+
+# Print
+iterator = my_list.head
+while iterator is not None:
+    print(iterator.data)
+    iterator = iterator.next
---
+++ class_instance.py
@@ -0,0 +1,21 @@
+class User:
+    def say_hello(some_user):
+        print("Hi! I'm {}!".format(some_user.name))
+
+user1 = User()
+user2 = User()
+user3 = User()
+
+user1.name = "김우진"
+user1.email = "woojin@codeit.kr"
+user1.password = "12345"
+
+user2.name = "강영훈"
+user2.email = "younghoon@codeit.kr"
+user2.password = "98765"
+
+user3.name = "최지웅"
+user3.email = "jiwoong@codeit.kr"
+user3.password = "78945"
+
+User.say_hello(user1)
