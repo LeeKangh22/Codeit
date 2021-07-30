4059cc1ebd22adde99d5d74acbd88b8479e02f94
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-07-30 14:03:46 +0900

    7/30 Double Linked List

--- DoubleLinkedList/DoubleLinkedList.py
+++ DoubleLinkedList/DoubleLinkedList.py
@@ -20,6 +20,37 @@ class DoubleLinkedList:
             self.tail.next = new_node
             new_node.prev = self.tail
             self.tail = new_node
+    
+    def insert_after(self, previous_node, data):
+        """링크드 리스트 추가 연산 메소드"""
+        # 코드를 쓰세요
+
+        new_node = Node(data)
+
+        if previous_node == self.tail:
+            previous_node.next = new_node
+            new_node.prev = previous_node
+            self.tail = new_node
+        else:
+            new_node.next = previous_node.next
+            new_node.prev = previous_node
+            previous_node.next = new_node
+            new_node.next.prev = new_node
+
+    def prepend(self, data):
+        """링크드 리스트 가장 앞에 데이터를 추가시켜주는 메소드"""
+        # 코드를 쓰세요
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = None
+            new_node.prev = None
+        else:
+            new_node.next = self.head
+            new_node.prev = None
+            self.head.prev = new_node
+            self.head = new_node
 
     def find_node_with_data(self, data):
         """링크드 리스트에서 탐색 연산 메소드. 단, 해당 노드가 없으면 None을 리턴한다"""
@@ -48,6 +79,24 @@ class DoubleLinkedList:
         else:
             self.tail.next = new_node  # 가장 마지막 노드 뒤에 새로운 노드를 추가하고
             self.tail = new_node  # 마지막 노드를 추가한 노드로 바꿔준다
+
+    def delete(self, node_to_delete):
+        """더블리 링크드 리스트 삭제 연산 메소드"""
+        # 코드를 쓰세요
+        if node_to_delete is self.head and node_to_delete is self.tail:
+            self.head = None
+            self.tail = None
+        elif node_to_delete is self.head and node_to_delete is not self.tail:
+            self.head = node_to_delete.next
+            self.head.prev = None
+        elif node_to_delete is self.tail and node_to_delete is not self.head:
+            self.tail = node_to_delete.prev
+            self.tail.next = None
+        elif node_to_delete is not self.head and node_to_delete is not self.tail:
+            node_to_delete.prev.next = node_to_delete.next
+            node_to_delete.next.prev = node_to_delete.prev
+        return node_to_delete.data
+
 
     def __str__(self):
         """링크드  리스트를 문자열로 표현해서 리턴하는 메소드"""
